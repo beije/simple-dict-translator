76cb8776e735c2bd274ca953ec938c2b425e6c9b
Benjamin Horn <git@benjaminhorn.se>
2018-02-06 14:59:55 +0200

    Updated tests to not reuse the source dict in the assertion (because of mutability)

--- test/translator.js
+++ test/translator.js
@@ -50,11 +50,11 @@ describe('translator', function () {
                     });
 
                     it('should translate correctly', function () {    
-                        expect(translator.translate('welcome', locale)).to.eql(dict.welcome);
+                        expect(translator.translate('welcome', locale)).to.eql('Välkommen');
                     });
 
                     it('should not become current translation', function () {
-                        expect(translator.translate('welcome')).to.not.eql(dict.welcome);
+                        expect(translator.translate('welcome')).to.not.eql('Welcome');
                     });
                 });
 
@@ -94,13 +94,13 @@ describe('translator', function () {
                 });
     
                 it('should be able to translate current locale', function () {
-                    expect(translator.translate('welcome')).to.eql(en_dict.welcome);
+                    expect(translator.translate('welcome')).to.eql('Welcome');
                 })
                 it('should be able to translate given locale', function () {
-                    expect(translator.translate('welcome', sv_locale)).to.eql(sv_dict.welcome);
+                    expect(translator.translate('welcome', sv_locale)).to.eql('Välkommen');
                 })
                 it('should be able to translate and go to fallback locale', function () {
-                    expect(translator.translate('goodbye')).to.eql(de_dict.goodbye);
+                    expect(translator.translate('goodbye')).to.eql('Auf Wiedersehen');
                 })
                 it('should be able to translate and give key if no matching translation was found', function () {
                     expect(translator.translate('hello')).to.eql('hello');
@@ -117,13 +117,13 @@ describe('translator', function () {
             });
 
             it('should be able to set/get current locale', function () {
-                translator.setCurrentLocale(sv_locale);
-                expect(translator.getCurrentLocale()).to.eql(sv_locale);
+                translator.setCurrentLocale('sv_se');
+                expect(translator.getCurrentLocale()).to.eql('sv_se');
             });
 
             it('should be able to set/get fallback locale', function () {
-                translator.setFallbackLocale(sv_locale);
-                expect(translator.getFallbackLocale()).to.eql(sv_locale);
+                translator.setFallbackLocale('sv_se');
+                expect(translator.getFallbackLocale()).to.eql('sv_se');
             });
         });
 
@@ -137,16 +137,16 @@ describe('translator', function () {
             });
 
             it('should return locale if locale is supported', function () {
-                expect(translator.getClosestLocale(en_locale)).to.eql(en_locale);
+                expect(translator.getClosestLocale(en_locale)).to.eql('en_us');
             })
             it('should return same language locale if no country is matched', function () {
-                expect(translator.getClosestLocale('en_gb')).to.eql(en_locale);
+                expect(translator.getClosestLocale('en_gb')).to.eql('en_us');
             })
             it('should return fallback if no match is found', function () {
-                expect(translator.getClosestLocale('fr_fr')).to.eql(de_locale);
+                expect(translator.getClosestLocale('fr_fr')).to.eql('de_de');
             })
             it('should normalize given locale, and return same', function () {
-                expect(translator.getClosestLocale('En-uS')).to.eql(en_locale);
+                expect(translator.getClosestLocale('En-uS')).to.eql('en_us');
             })
 
         });
